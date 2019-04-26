51efb1558ce4013a2f856590ace9272a27ea884d
modulitos <you@example.com>
2019-04-26 01:13:10 -0700

    finish ch2-p6

--- src/bin/ch2-p6_check-palindrome.rs
+++ src/bin/ch2-p6_check-palindrome.rs
@@ -3,15 +3,33 @@
 extern crate cracking;
 use cracking::LinkedList;
 
-
 pub trait CheckPalindrome<T> {
     fn is_palindrome(&self) -> bool;
 }
 
-// Assumes that both lists are of the same size.
-impl<T> CheckPalindrome<T> for LinkedList<T> {
+// Assumes that LinkedList has implemented a doubleendediterator
+// (next_back method)
+impl<T> CheckPalindrome<T> for LinkedList<T>
+where
+    T: std::cmp::Eq
+        + std::hash::Hash
+        + std::clone::Clone
+        + std::cmp::PartialOrd
+        + std::cmp::PartialOrd
+        + std::fmt::Debug,
+{
     fn is_palindrome(&self) -> bool {
-        false
+        let mut iter = self.iter();
+        while let Some(next) = iter.next() {
+            // println!("checking next: {:?}", next);
+            if let Some(last) = iter.next_back() {
+                // println!("comparing next with last: {:?}", last);
+                if next.borrow().data != last.borrow().data {
+                    return false;
+                }
+            }
+        }
+        true
     }
 }
 
@@ -19,11 +37,27 @@ mod test {
     use super::*;
 
     #[test]
-    fn eq() {
+    fn check_palindrome() {
         let mut list = LinkedList::new();
         list.append('a');
         list.append('b');
         list.append('c');
+        assert!(!list.is_palindrome());
+        list.append('c');
+        list.append('b');
+        list.append('a');
+        assert!(list.is_palindrome());
+    }
+    #[test]
+    fn check_odd_palindrome() {
+        let mut list = LinkedList::new();
+        list.append('a');
+        list.append('b');
+        list.append('c');
+        list.append('b');
+        list.append('a');
+        assert!(list.is_palindrome());
+        list.append('a');
         assert!(!list.is_palindrome());
     }
 
