571d441c668932f1fe9d79113d421276679b0378
modulitos <you@example.com>
2019-04-26 01:48:16 -0700

    ch2 p06: add iterative, stack-based implementation

--- src/bin/ch2-p6_check-palindrome.rs
+++ src/bin/ch2-p6_check-palindrome.rs
@@ -2,9 +2,11 @@
 // palindrome.
 extern crate cracking;
 use cracking::LinkedList;
+use std::vec::Vec;
 
 pub trait CheckPalindrome<T> {
     fn is_palindrome(&self) -> bool;
+    fn is_palindrome_iterative(&self) -> bool;
 }
 
 // Assumes that LinkedList has implemented a doubleendediterator
@@ -31,6 +33,32 @@ where
         }
         true
     }
+
+    fn is_palindrome_iterative(&self) -> bool {
+        let mut iter_fast = self.iter();
+        let mut iter_slow = self.iter();
+        let mut stack: Vec<T> = Vec::new();
+        let mut is_odd = false;
+        while let Some(_) = iter_fast.next() {
+            let next_slow = iter_slow.next().unwrap();
+            stack.push(next_slow.borrow().data.clone());
+            if let None = iter_fast.next() {
+                // we have an odd number of nodes
+                is_odd = true;
+            }
+        }
+        // now we know slow iterator is at node len/2
+        if is_odd {
+            stack.pop();
+        }
+        while let Some(next) = iter_slow.next() {
+            let next_data = stack.pop().unwrap();
+            if next_data != next.borrow().data {
+                return false;
+            }
+        }
+        true
+    }
 }
 
 mod test {
@@ -48,6 +76,19 @@ mod test {
         list.append('a');
         assert!(list.is_palindrome());
     }
+
+    #[test]
+    fn check_palindrome_iterative() {
+        let mut list = LinkedList::new();
+        list.append('a');
+        list.append('b');
+        list.append('c');
+        assert!(!list.is_palindrome_iterative());
+        list.append('c');
+        list.append('b');
+        list.append('a');
+        assert!(list.is_palindrome_iterative());
+    }
     #[test]
     fn check_odd_palindrome() {
         let mut list = LinkedList::new();
@@ -59,6 +100,18 @@ mod test {
         assert!(list.is_palindrome());
         list.append('a');
         assert!(!list.is_palindrome());
+    }
+    #[test]
+    fn check_odd_palindrome_iterative() {
+        let mut list = LinkedList::new();
+        list.append('a');
+        list.append('b');
+        list.append('c');
+        list.append('b');
+        list.append('a');
+        assert!(list.is_palindrome_iterative());
+        list.append('a');
+        assert!(!list.is_palindrome_iterative());
     }
 
 }
