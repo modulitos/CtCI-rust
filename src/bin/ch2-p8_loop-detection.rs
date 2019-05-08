dcf42ff378bc61111426d7550dedd8966a34cc1b
modulitos <you@example.com>
2019-05-08 01:30:43 -0700

    ch2 p08 smarter loop detection

--- src/bin/ch2-p8_loop-detection.rs
+++ src/bin/ch2-p8_loop-detection.rs
@@ -7,15 +7,16 @@
 // they are intersecting.
 
 extern crate cracking;
-use cracking::{SinglyLinkedList as LinkedList, Node, NodeRef, HashedNode};
+use cracking::{HashedNode, Node, NodeRef, SinglyLinkedList as LinkedList};
 
 use std::collections::HashSet;
 
-use std::rc::Rc;
 use std::cell::RefCell;
+use std::rc::Rc;
 
 pub trait LoopDetection<T> {
     fn detect_loop(&self) -> Option<NodeRef<T>>;
+    fn detect_loop_with_set(&self) -> Option<NodeRef<T>>;
 }
 
 impl<T> LoopDetection<T> for LinkedList<T>
@@ -27,7 +28,51 @@ where
         + std::cmp::PartialOrd
         + std::fmt::Debug,
 {
-    fn detect_loop(&self) -> Option<NodeRef<T>>{
+    fn detect_loop(&self) -> Option<NodeRef<T>> {
+        let mut slow_iter = self.iter();
+        let mut fast_iter = self.iter();
+        while let (Some(slow), Some(_)) = (slow_iter.next(), fast_iter.next()) {
+            match fast_iter.next() {
+                // fast_iter moves 2 steps per iteration.
+                None => return None,
+                Some(fast) => {
+                    if Rc::ptr_eq(&slow, &fast) {
+                        // loop is detected here - this is our collision point.
+
+                        // If the head is k steps from the start of
+                        // the loop, then when slow_iter reaches the
+                        // start of the loop, fast_iter is k steps
+                        // ahead of slow_iter.
+
+                        // If `K = k % loop_size`, then fast_iter is K
+                        // steps ahead of slow iter, and loop_size - K
+                        // steps behind slow iter. So slow_iter will
+                        // move loop_size - K steps once they collide,
+                        // making the collision point K steps behind
+                        // the start of he loop. This is the same as
+                        // being k steps behind the start of the loop!
+
+                        // Thus, we know that the collision point is
+                        // the same number of nodes away from the loop
+                        // start as the head, so let's set a new
+                        // pointer to the head, and move along both
+                        // iterators until they collide again at the
+                        // start of the loop.
+                        slow_iter = self.iter();
+                        while let (Some(slow), Some(fast)) = (slow_iter.next(), fast_iter.next()) {
+                            if Rc::ptr_eq(&slow, &fast) {
+                                // This is the start of our loop.
+                                return Some(fast);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        None
+    }
+
+    fn detect_loop_with_set(&self) -> Option<NodeRef<T>> {
         let mut visited_nodes: HashSet<HashedNode<T>> = HashSet::new();
         for node in self.iter() {
             if visited_nodes.contains(&HashedNode::from_node(node.clone())) {
@@ -40,7 +85,6 @@ where
     }
 }
 
-
 mod test {
     use super::*;
 
@@ -48,6 +92,7 @@ mod test {
     fn detect_loop_empty() {
         let list: LinkedList<i32> = LinkedList::new();
         assert_eq!(list.detect_loop(), None);
+        assert_eq!(list.detect_loop_with_set(), None);
     }
 
     #[test]
@@ -57,6 +102,7 @@ mod test {
         list.append(2);
         list.append(3);
         assert_eq!(list.detect_loop(), None);
+        assert_eq!(list.detect_loop_with_set(), None);
     }
 
     #[test]
@@ -64,16 +110,57 @@ mod test {
         let mut list: LinkedList<i32> = LinkedList::new();
         list.append(1);
         assert_eq!(list.detect_loop(), None);
+        assert_eq!(list.detect_loop_with_set(), None);
     }
 
     #[test]
     fn detect_loop_even() {
-        let node = Rc::new(RefCell::new(Node{ data: 9, next: None}));
+        let node = Rc::new(RefCell::new(Node {
+            data: 9,
+            next: None,
+        }));
         let mut list: LinkedList<i32> = LinkedList::new();
         list.append_node(node.clone());
         list.append(1);
         list.append(2);
         list.append(3);
+        list.append_node(node.clone());
+        assert!(Rc::ptr_eq(&list.detect_loop().unwrap(), &node));
+        assert!(Rc::ptr_eq(&list.detect_loop_with_set().unwrap(), &node));
+    }
+
+    #[test]
+    fn detect_loop_odd() {
+        let node = Rc::new(RefCell::new(Node {
+            data: 9,
+            next: None,
+        }));
+        let mut list: LinkedList<i32> = LinkedList::new();
+        list.append_node(node.clone());
+        list.append(1);
+        list.append(2);
+        list.append(3);
+        list.append(4);
+        list.append_node(node.clone());
+        assert!(Rc::ptr_eq(&list.detect_loop().unwrap(), &node));
+    }
+
+    #[test]
+    fn detect_loop_with_tail() {
+        let node = Rc::new(RefCell::new(Node {
+            data: 9,
+            next: None,
+        }));
+        let mut list: LinkedList<i32> = LinkedList::new();
+        list.append(1);
+        list.append(2);
+        list.append(3);
+        list.append_node(node.clone());
+        list.append(4);
+        list.append(5);
+        list.append(6);
+        list.append(7);
+        list.append(8);
         list.append_node(node.clone());
         assert!(Rc::ptr_eq(&list.detect_loop().unwrap(), &node));
     }
