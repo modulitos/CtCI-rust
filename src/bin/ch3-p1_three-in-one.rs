d6b7dca1c61996eb4f2aca849858fdf5a4a28971
modulitos <you@example.com>
2019-05-27 19:01:08 -0700

    ch3-p1 push

--- src/bin/ch3-p1_three-in-one.rs
+++ src/bin/ch3-p1_three-in-one.rs
@@ -3,27 +3,43 @@ use std::cmp;
 
 type Node<T> = Option<T>;
 
-pub struct ArrayList<T> {
+const DEFAULT_CAP: usize = 10;
+
+pub struct ThreeStacks<T> {
     buf: Box<[Node<T>]>,
     cap: usize,
-    pub length: usize,
+    indexes: [usize; 3],
 }
 
-impl<T> ArrayList<T>
+impl<T> ThreeStacks<T>
 where
     T: std::clone::Clone,
 {
     pub fn new() -> Self {
         Self {
-            buf: vec![None; 10].into_boxed_slice(),
-            cap: 10,
-            length: 0,
+            buf: vec![None; DEFAULT_CAP].into_boxed_slice(),
+            cap: DEFAULT_CAP,
+            indexes: [0, DEFAULT_CAP / 3, (DEFAULT_CAP / 3) * 2],
         }
+    }
+
+    pub fn push(&mut self, value: T, stack: usize) -> Result<(), &str>{
+        // TODO: If stack is full, we'll need to grow and copy/paste the data over.
+        if stack > self.indexes.len() {
+            return Err("wrong stack reference!");
+        }
+        let index = self.indexes[stack];
+        if index > (index * ((self.cap + 1) / 3)) {
+            return Err("out of room!");
+        }
+        self.buf[index] = Some(value);
+        return Ok(());
     }
 
     fn grow(&mut self, min_cap: usize) {
         let old_cap = self.buf.len();
         let mut new_cap = old_cap + (old_cap >> 1);
+        println!("newcap: {}", new_cap);
         new_cap = cmp::max(new_cap, min_cap);
         new_cap = cmp::min(new_cap, usize::max_value());
         let current = self.buf.clone();
@@ -37,9 +53,16 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_array_resizing() {
+    fn create() {
         assert!(true);
-        let a: ArrayList<u64> = ArrayList::new();
+        let a: ThreeStacks<u64> = ThreeStacks::new();
+        assert!(true);
+    }
+
+    #[test]
+    fn insert_one_item() {
+        let mut s: ThreeStacks<u64> = ThreeStacks::new();
+        s.push(5, 0);
         assert!(true);
     }
 }
