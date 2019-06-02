c24e2fce2f6fff945ce7e2b0bbdc61d07641311f
modulitos <you@example.com>
2019-06-02 01:51:48 -0700

    ch3-p4 implement peek()

--- src/bin/ch1-p4_queue-via-stacks.rs
+++ src/bin/ch1-p4_queue-via-stacks.rs
@@ -23,13 +23,22 @@ where
         self.incoming.push(value);
     }
 
-    fn pop(&mut self) -> Option<T> {
+    fn _shift_elements(&mut self) {
         if self.outgoing.is_empty() && !self.incoming.is_empty() {
             while let Some(value) = self.incoming.pop() {
                 self.outgoing.push(value);
             }
         }
+    }
+
+    fn pop(&mut self) -> Option<T> {
+        self._shift_elements();
         self.outgoing.pop()
+    }
+
+    fn peek(&mut self) -> Option<T> {
+        self._shift_elements();
+        self.outgoing.peek()
     }
 }
 
@@ -60,6 +69,26 @@ mod test {
         s.push(5);
         s.push(6);
         assert_eq!(s.pop(), Some(2));
+        assert_eq!(s.pop(), Some(3));
+        assert_eq!(s.pop(), Some(4));
+        assert_eq!(s.pop(), Some(5));
+        assert_eq!(s.pop(), Some(6));
+    }
+
+    #[test]
+    fn peek() {
+        let mut s: MyQueue<u64> = MyQueue::new();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        assert_eq!(s.peek(), Some(1));
+        assert_eq!(s.pop(), Some(1));
+        s.push(4);
+        s.push(5);
+        s.push(6);
+        assert_eq!(s.peek(), Some(2));
+        assert_eq!(s.pop(), Some(2));
+        assert_eq!(s.peek(), Some(3));
         assert_eq!(s.pop(), Some(3));
         assert_eq!(s.pop(), Some(4));
         assert_eq!(s.pop(), Some(5));
