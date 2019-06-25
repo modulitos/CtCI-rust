f79ee275000ed4c8c94ae509c09e771d3d45141a
modulitos <you@example.com>
2019-06-25 11:22:40 -0700

    ch4-p5 more tests

--- src/bin/ch4-p5_validate-bst.rs
+++ src/bin/ch4-p5_validate-bst.rs
@@ -30,12 +30,10 @@ where
                         && l.data < n.data
                         && n.data < r.data
                 }
-                (None, Some(r)) => {
-                    self._is_tree_node_valid(&n.right) && n.data < r.data
-                }
+                (None, Some(r)) => self._is_tree_node_valid(&n.right) && n.data < r.data,
                 (Some(l), None) => self._is_tree_node_valid(&n.left) && l.data < n.data,
                 (None, None) => true,
-            }
+            };
         }
         true
     }
@@ -58,6 +56,71 @@ mod tests {
         let mut root = TreeNode::<u32>::new_node(2);
         root.set_left(TreeNode::new_node(3));
         root.set_right(TreeNode::new_node(3));
+        let t = BinarySearchTree::<u32>::new().with_root(root).create();
+        assert_eq!(t.validate_bst(), false);
+    }
+
+    #[test]
+    fn valid_tree_random() {
+        let mut t = BinarySearchTree::<u32>::new();
+        t.add(5);
+        t.add(1);
+        t.add(2);
+        t.add(3);
+        t.add(4);
+        t.add(9);
+        t.add(7);
+        assert_eq!(t.validate_bst(), true);
+    }
+
+    #[test]
+    fn valid_tree_unbalanced() {
+        let mut t = BinarySearchTree::<u32>::new();
+        t.add(1);
+        t.add(2);
+        t.add(3);
+        t.add(4);
+        t.add(5);
+        t.add(6);
+        t.add(7);
+        assert_eq!(t.validate_bst(), true);
+    }
+
+    #[test]
+    fn valid_tree_balanced() {
+        let mut t = BinarySearchTree::<u32>::new();
+        t.add(5);
+        // root.left
+        t.add(2);
+        t.add(1);
+        t.add(0);
+        t.add(3);
+        t.add(4);
+        // root.right
+        t.add(9);
+        t.add(7);
+        t.add(6);
+        t.add(8);
+        t.add(13);
+        t.add(11);
+        t.add(12);
+        t.add(10);
+        t.add(15);
+        t.add(16);
+        t.add(14);
+        assert_eq!(t.validate_bst(), true);
+    }
+
+    #[test]
+    fn invalid_complex_tree() {
+        let mut root = TreeNode::<u32>::new_node(3);
+        let mut root_right = TreeNode::<u32>::new_node(9);
+        root_right.set_right(TreeNode::new_node(7));  // out of order!
+        let mut root_left = TreeNode::<u32>::new_node(2);
+        root_left.set_left(TreeNode::new_node(1));
+        root_left.set_right(TreeNode::new_node(3));
+        root.set_left(root_left);
+        root.set_right(root_right);
         let t = BinarySearchTree::<u32>::new().with_root(root).create();
         assert_eq!(t.validate_bst(), false);
     }
