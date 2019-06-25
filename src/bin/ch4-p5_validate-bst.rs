50a3a4b1449b3194916375115e5099ef087b2d72
modulitos <you@example.com>
2019-06-25 11:13:54 -0700

    ch4-p5 done

--- src/bin/ch4-p5_validate-bst.rs
+++ src/bin/ch4-p5_validate-bst.rs
@@ -1,12 +1,12 @@
 // Validate BST: Implement a function to check if a binary tree is a
 // binary search tree.
 
-use cracking::{BinarySearchTree, TreeNode};
+use cracking::{BinarySearchTree, Tree, TreeNode};
 
 trait ValidateBST<T> {
     fn validate_bst(&self) -> bool;
 
-    // fn validate_in_order(&self, node: &Tree<T>, prev: T) -> bool;
+    fn _is_tree_node_valid(&self, node: &Tree<T>) -> bool;
 }
 
 impl<T> ValidateBST<T> for BinarySearchTree<T>
@@ -14,6 +14,29 @@ where
     T: std::cmp::PartialOrd + std::clone::Clone + std::fmt::Debug,
 {
     fn validate_bst(&self) -> bool {
+        self._is_tree_node_valid(&self.root)
+    }
+
+    fn _is_tree_node_valid(&self, node: &Tree<T>) -> bool {
+        // A BST is valid if its left and right subtrees are valid,
+        // and if the value of the left child is less than the
+        // current, and the value of the right child is more than the
+        // current
+        if let Some(n) = node {
+            return match (&n.left, &n.right) {
+                (Some(l), Some(r)) => {
+                    self._is_tree_node_valid(&n.left)
+                        && self._is_tree_node_valid(&n.right)
+                        && l.data < n.data
+                        && n.data < r.data
+                }
+                (None, Some(r)) => {
+                    self._is_tree_node_valid(&n.right) && n.data < r.data
+                }
+                (Some(l), None) => self._is_tree_node_valid(&n.left) && l.data < n.data,
+                (None, None) => true,
+            }
+        }
         true
     }
 }
@@ -30,12 +53,12 @@ mod tests {
         assert_eq!(t.validate_bst(), true);
     }
 
-    // #[test]
-    // fn simple_invalid_tree() {
-    //     let mut root = TreeNode::<u32>::new_node(2);
-    //     root.set_left(TreeNode::new_node(3));
-    //     root.set_right(TreeNode::new_node(3));
-    //     let t = BinarySearchTree::<u32>::new().with_root(root).create();
-    //     assert_eq!(t.validate_bst(), true);
-    // }
+    #[test]
+    fn simple_invalid_tree() {
+        let mut root = TreeNode::<u32>::new_node(2);
+        root.set_left(TreeNode::new_node(3));
+        root.set_right(TreeNode::new_node(3));
+        let t = BinarySearchTree::<u32>::new().with_root(root).create();
+        assert_eq!(t.validate_bst(), false);
+    }
 }
