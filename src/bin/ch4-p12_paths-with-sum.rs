318a149b85448ba847fa218b7dacf032a23ec16e
modulitos <you@example.com>
2019-08-11 10:20:43 -0700

    ch4-p12 optimized solution

--- src/bin/ch4-p12_paths-with-sum.rs
+++ src/bin/ch4-p12_paths-with-sum.rs
@@ -9,6 +9,7 @@
 // space requirements? Can we use a hash table of size O(n)?
 
 use cracking::{BTree as Tree, BTreeNode as Node};
+use std::collections::HashMap;
 
 // Counts paths adding to the sum, starting at the given node:
 fn count_paths_from_node<T>(node: &Tree<T>, sum: i8) -> i8
@@ -33,6 +34,7 @@ where
     counts
 }
 
+// Time: O(n * log(n))
 fn count_paths<T>(node: &Tree<T>, sum: i8) -> i8
 where
     T: Copy,
@@ -51,6 +53,78 @@ where
     // count all paths from the nodes below this node:
     counts += count_paths(&n.left, sum);
     counts += count_paths(&n.right, sum);
+    counts
+}
+
+// space: O(log(n))  on a balanced tree, O(n) on unbalanced tree
+// time: O(n)
+fn count_paths_optimized<T>(node: &Tree<T>, sum: i8) -> i8
+where
+    T: Copy + std::hash::Hash + std::cmp::Eq + std::fmt::Debug,
+    i8: std::convert::From<T>,
+{
+    let mut map = HashMap::new();
+    map.insert(0, 1);
+    count_paths_optimized_rec(node, sum, 0, &mut map)
+}
+
+fn count_paths_optimized_rec<T>(
+    node: &Tree<T>,
+    sum: i8,
+    mut running_sum: i8,
+    // TODO: what is the difference between ownership and mutable reference here???
+    map: &mut HashMap<i8, i8>,
+) -> i8
+where
+    T: Copy + std::hash::Hash + std::cmp::Eq + std::fmt::Debug,
+    i8: std::convert::From<T>,
+{
+    if node.is_none() {
+        return 0;
+    }
+
+    let n = node.as_ref().unwrap();
+    let data = i8::from(n.data);
+    running_sum += data;
+
+    println!();
+    println!("checking node: {:?}", node);
+
+    let mut counts = 0;
+    if !map.contains_key(&running_sum) {
+        map.insert(running_sum, 1);
+    } else {
+        map.insert(running_sum, map.get(&running_sum).unwrap() + 1);
+    }
+    println!("map: {:?}", map);
+    println!("running sum: {}", running_sum);
+    println!(
+        "checking if map contains runningsum - sum: {}",
+        running_sum - sum
+    );
+    if map.contains_key(&(running_sum - sum)) {
+        // we found a match!
+        println!(
+            "we found a match! running sum: {}, sum: {}",
+            running_sum, sum
+        );
+        counts += map.get(&(running_sum - sum)).unwrap()
+    }
+
+    // count all paths from the nodes below this node:
+    counts += count_paths_optimized_rec(&n.left, sum, running_sum, map);
+    counts += count_paths_optimized_rec(&n.right, sum, running_sum, map);
+
+    // Reset the hashmap for this node:
+    if *map.get(&running_sum).unwrap() == 1 {
+        map.remove(&running_sum);
+    } else {
+        if let Some(value) = map.get(&running_sum) {
+            map.insert(running_sum, value - 1);
+        } else {
+            panic!("no value for map at sum: {}", running_sum);
+        }
+    }
     counts
 }
 
@@ -83,13 +157,14 @@ fn test_count_paths_simple() {
             Node::new(3, Node::new(2, None, None), None),
             Node::new(-2, None, None),
         ),
-        Node::new(1, None, Node::new(2, None, None)),
+        Node::new(8, None, Node::new(2, None, None)),
     );
     assert_eq!(count_paths(&node_fixture, 8), 3);
+    assert_eq!(count_paths_optimized(&node_fixture, 8), 3);
 }
 
 #[test]
-fn test_count_paths() {
+fn test_count_paths_complex() {
     let node_fixture = Node::<i8>::new(
         10,
         Node::new(
@@ -100,4 +175,5 @@ fn test_count_paths() {
         Node::new(-3, None, Node::new(11, None, None)),
     );
     assert_eq!(count_paths(&node_fixture, 8), 3);
+    assert_eq!(count_paths_optimized(&node_fixture, 8), 3);
 }
