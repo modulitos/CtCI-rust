6f1c768f350134bd4c9f61b131869390f3b7a96c
modulitos <you@example.com>
2019-09-29 15:12:46 -0700

    ch7-p2 implement Employee/Rank ordering

--- src/bin/ch7-p2_call-center.rs
+++ src/bin/ch7-p2_call-center.rs
@@ -8,18 +8,20 @@
 // dispatchCall() which assigns a call to the first available
 // employee.
 
-#[derive(Debug, PartialEq, Eq)]
-enum Role {
+use core::cmp::Ordering;
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+enum Rank {
     Respondent,
     Manager,
     Director,
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Ord)]
 struct Employee {
-    is_available: bool,
-    role: Role,
+    rank: Rank,
     name: String,
+    is_available: bool,
 }
 
 impl Employee {
@@ -28,14 +30,32 @@ impl Employee {
     }
 }
 
+// Without this, Employee ordering would be determined by the order of
+// the comparson of attributes in the struct, in whatever order they
+// are defined.
+impl PartialOrd for Employee {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.rank.partial_cmp(&other.rank)
+    }
+}
+
+struct Call {
+    from: String,
+    minimum_rank: Rank,
+}
+
 struct CallCenter {
     employees: Vec<Employee>,
 }
 
 impl CallCenter {
-    fn _find_available_employee_idx(&self, role: Role) -> Option<usize> {
+    fn new(mut employees: Vec<Employee>) -> Self {
+        employees.sort();
+        CallCenter { employees }
+    }
+    fn _find_available_employee_idx(&self, call: Call) -> Option<usize> {
         self.employees.iter().enumerate().find_map(|(i, e)| {
-            if e.role == role && e.is_available {
+            if e.rank >= call.minimum_rank && e.is_available {
                 Some(i)
             } else {
                 None
@@ -43,11 +63,10 @@ impl CallCenter {
         })
     }
 
-    fn dispatch_call(&mut self) -> Option<&Employee> {
-        if let Some(employee_idx) = self
-            ._find_available_employee_idx(Role::Respondent)
-            .or_else(|| self._find_available_employee_idx(Role::Manager))
-            .or_else(|| self._find_available_employee_idx(Role::Director))
+    fn dispatch_call(&mut self, call: Call) -> Option<&Employee> {
+        if let Some(employee_idx) = self._find_available_employee_idx(call)
+        // .or_else(|| self._find_available_employee_idx(Rank::Manager))
+        // .or_else(|| self._find_available_employee_idx(Rank::Director))
         {
             let employee = &mut self.employees[employee_idx];
             employee.set_busy();
@@ -59,14 +78,101 @@ impl CallCenter {
 }
 
 #[test]
+fn test_rank_ordering() {
+    assert_eq!(Rank::Manager > Rank::Director, false);
+    assert_eq!(Rank::Manager > Rank::Respondent, true);
+}
+
+#[test]
+fn test_employee_ordering() {
+    // assert that Rank is the only determination for ordering, and
+    // that swapping names does not affect ordering.
+    assert_eq!(
+        Employee {
+            rank: Rank::Manager,
+            is_available: false,
+            name: String::from("joe")
+        } > Employee {
+            rank: Rank::Director,
+            is_available: false,
+            name: String::from("kyle")
+        },
+        false
+    );
+    assert_eq!(
+        Employee {
+            rank: Rank::Manager,
+            is_available: false,
+            name: String::from("kyle")
+        } > Employee {
+            rank: Rank::Director,
+            is_available: false,
+            name: String::from("joe")
+        },
+        false
+    );
+    assert_eq!(
+        Employee {
+            rank: Rank::Manager,
+            is_available: false,
+            name: String::from("kyle")
+        } < Employee {
+            rank: Rank::Director,
+            is_available: false,
+            name: String::from("joe")
+        },
+        true
+    );
+
+    assert_eq!(
+        Employee {
+            rank: Rank::Manager,
+            is_available: false,
+            name: String::from("joe")
+        } < Employee {
+            rank: Rank::Director,
+            is_available: false,
+            name: String::from("kyle")
+        },
+        true
+    );
+    // assert that 2 employess with the same rank are neither greater
+    // than nor less than each other
+    assert_eq!(
+        Employee {
+            rank: Rank::Manager,
+            is_available: false,
+            name: String::from("kyle")
+        } < Employee {
+            rank: Rank::Manager,
+            is_available: false,
+            name: String::from("joe")
+        },
+        false
+    );
+    assert_eq!(
+        Employee {
+            rank: Rank::Manager,
+            is_available: false,
+            name: String::from("kyle")
+        } > Employee {
+            rank: Rank::Manager,
+            is_available: false,
+            name: String::from("joe")
+        },
+        false
+    );
+}
+
+#[test]
 fn test_employees() {
     let mut employees = vec![Employee {
-        role: Role::Respondent,
+        rank: Rank::Respondent,
         is_available: true,
         name: String::from("joe"),
     }];
     employees.push(Employee {
-        role: Role::Manager,
+        rank: Rank::Manager,
         is_available: false,
         name: String::from("manager Bill"),
     });
@@ -80,61 +186,165 @@ fn test_employees() {
 fn test_call_center_none() {
     let employees = vec![
         Employee {
-            role: Role::Director,
+            rank: Rank::Director,
             is_available: false,
             name: String::from("Jane"),
         },
         Employee {
-            role: Role::Respondent,
+            rank: Rank::Respondent,
             is_available: false,
             name: String::from("Joe"),
         },
     ];
     let mut call_center = CallCenter { employees };
-    assert_eq!(call_center.dispatch_call(), None);
+    assert_eq!(
+        call_center.dispatch_call(Call {
+            from: String::from("somebody"),
+            minimum_rank: Rank::Respondent,
+        }),
+        None
+    );
 }
 
 #[test]
 fn test_call_center_single() {
     let employees = vec![Employee {
-        role: Role::Director,
+        rank: Rank::Director,
         is_available: true,
         name: String::from("Jane"),
     }];
     let mut call_center = CallCenter { employees };
     assert_eq!(
-        call_center.dispatch_call().unwrap().name,
+        call_center
+            .dispatch_call(Call {
+                from: String::from("someone"),
+                minimum_rank: Rank::Respondent,
+            })
+            .unwrap()
+            .name,
         String::from("Jane")
     );
 }
 
-#[test]
-fn test_call_center_multiple() {
-    let employees = vec![
-        Employee {
-            role: Role::Director,
-            is_available: true,
-            name: String::from("Jane"),
-        },
-        Employee {
-            role: Role::Respondent,
-            is_available: true,
-            name: String::from("Jerry"),
-        },
-    ];
-    let mut call_center = CallCenter { employees };
-    assert_eq!(
-        call_center.dispatch_call().unwrap().name,
-        String::from("Jerry")
-    );
-}
+// #[test]
+// fn test_call_center_respondent() {
+//     let employees = vec![
+//         Employee {
+//             rank: Rank::Director,
+//             is_available: true,
+//             name: String::from("Diane Director"),
+//         },
+//         Employee {
+//             rank: Rank::Manager,
+//             is_available: true,
+//             name: String::from("Marty Manager"),
+//         },
+//         Employee {
+//             rank: Rank::Respondent,
+//             is_available: false,
+//             name: String::from("Joe"),
+//         },
+//         Employee {
+//             rank: Rank::Respondent,
+//             is_available: true,
+//             name: String::from("Jane"),
+//         },
+//     ];
+//     let mut call_center = CallCenter { employees };
+//     assert_eq!(
+//         call_center
+//             .dispatch_call(Call {
+//                 from: String::from("Luke"),
+//                 minimum_rank: Rank::Respondent
+//             })
+//             .unwrap()
+//             .name,
+//         String::from("Jane")
+//     );
+// }
+
+// #[test]
+// fn test_call_center_director() {
+//     let employees = vec![
+//         Employee {
+//             rank: Rank::Director,
+//             is_available: true,
+//             name: String::from("Diane Director"),
+//         },
+//         Employee {
+//             rank: Rank::Manager,
+//             is_available: true,
+//             name: String::from("Marty Manager"),
+//         },
+//         Employee {
+//             rank: Rank::Respondent,
+//             is_available: false,
+//             name: String::from("Joe"),
+//         },
+//         Employee {
+//             rank: Rank::Respondent,
+//             is_available: true,
+//             name: String::from("Jane"),
+//         },
+//     ];
+//     let mut call_center = CallCenter { employees };
+//     assert_eq!(
+//         call_center
+//             .dispatch_call(Call {
+//                 from: String::from("Obama"),
+//                 minimum_rank: Rank::Respondent
+//             })
+//             .unwrap()
+//             .name,
+//         String::from("Diane Director")
+//     );
+// }
+
+// fn test_call_center_manager() {
+//     let employees = vec![
+//         Employee {
+//             rank: Rank::Director,
+//             is_available: true,
+//             name: String::from("Diane Director"),
+//         },
+//         Employee {
+//             rank: Rank::Manager,
+//             is_available: true,
+//             name: String::from("Marty Manager"),
+//         },
+//         Employee {
+//             rank: Rank::Respondent,
+//             is_available: false,
+//             name: String::from("Joe"),
+//         },
+//         Employee {
+//             rank: Rank::Respondent,
+//             is_available: true,
+//             name: String::from("Jane"),
+//         },
+//     ];
+//     let mut call_center = CallCenter { employees };
+//     assert_eq!(
+//         call_center
+//             .dispatch_call(Call {
+//                 from: String::from("Middle Manager"),
+//                 minimum_rank: Rank::Respondent
+//             })
+//             .unwrap()
+//             .name,
+//         String::from("Marty Manager")
+//     );
+// }
 fn main() {
     let mut call_center = CallCenter {
         employees: vec![Employee {
-            role: Role::Director,
+            rank: Rank::Director,
             is_available: false,
             name: String::from("Mr. Director"),
         }],
     };
-    call_center.dispatch_call();
+    call_center.dispatch_call(Call {
+        from: String::from("Obama"),
+        minimum_rank: Rank::Respondent,
+    });
 }
